93d514c365b10c2ab42ebd6ee70e863e9736ecad
laibashamshad <shamshadlaiba313@gmail.com>
2024-07-25 02:58:10 -0700

    project

---
+++ project.js
@@ -0,0 +1,10 @@
+let stdname ="laiba"
+alert("my name is"+ stdname)
+let city ="peshawar"
+alert("my city is" + city)
+let numOne=5
+let numTwo=8
+alert(numOne+numTwo)
+let personName="laiba"
+let weight=45
+alert("my name is "+personName +" my weight is "+weight)
